a9d2671e914bb02085cb43a044394cbd93831708
moonstar-x <christian.lopez99@outlook.com>
2023-10-27 15:29:10 -0500

    Added support for svg imports.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,6 +20,7 @@ export default defineConfig({
   },
   plugins: [
     tsconfigPaths(),
+    svgr(),
     react()
   ]
 });
