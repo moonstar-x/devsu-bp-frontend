50d81db0a4fd0e501971907137fd46f103847fe7
moonstar-x <christian.lopez99@outlook.com>
2023-10-27 12:54:31 -0500

    Added react-query.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Router } from '$components/router';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      retry: false
+    }
+  }
+});
 
 export const App = () => {
   return (
-    <Router />
+    <QueryClientProvider client={queryClient}>
+      <Router />
+
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
