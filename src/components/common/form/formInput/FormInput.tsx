6140c80fd91952d5cb9483aa68d42bc9ce321003
moonstar-x <christian.lopez99@outlook.com>
2023-10-28 13:20:50 -0500

    Added styling for form input component.

--- src/components/common/form/formInput/FormInput.tsx
+++ src/components/common/form/formInput/FormInput.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.scss';
 
 interface Props extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
   label?: string
   feedback?: string
 }
 
-export const FormInput = React.forwardRef<HTMLInputElement, Props>(({ ...props }, ref) => {
+export const FormInput = React.forwardRef<HTMLInputElement, Props>(({ label, feedback, className, ...props }, ref) => {
   return (
-    <input ref={ref} {...props} />
+    <div className={styles.field}>
+      {
+        label && (
+          <label>
+            {label}
+          </label>
+        )
+      }
+      <input className={clsx(styles.input, { [styles.inputError]: !!feedback }, className)} ref={ref} {...props} />
+
+      {
+        feedback && (
+          <span className={styles.feedback}>
+            {feedback}
+          </span>
+        )
+      }
+    </div>
   );
 });
