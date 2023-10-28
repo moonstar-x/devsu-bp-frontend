58481d332a644373f36fc264a6ec983525acdb34
moonstar-x <christian.lopez99@outlook.com>
2023-10-28 04:27:36 -0500

    FormInput now uses forwardRef.

--- src/components/common/form/formInput/FormInput.tsx
+++ src/components/common/form/formInput/FormInput.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 interface Props extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
-
+  label?: string
+  feedback?: string
 }
 
-export const FormInput: React.FC<Props> = ({ ...props }) => {
+export const FormInput = React.forwardRef<HTMLInputElement, Props>(({ ...props }, ref) => {
   return (
-    <input {...props} />
+    <input ref={ref} {...props} />
   );
-};
+});
