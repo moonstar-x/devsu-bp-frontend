dfecde5b0e43e3dfc6afda1aa90079f1140823fa
moonstar-x <christian.lopez99@outlook.com>
2023-10-29 16:03:32 -0500

    Added ProductTable to ProductSearchPage.

--- src/components/pages/ProductSearchPage.tsx
+++ src/components/pages/ProductSearchPage.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 import { PageWrapper } from '$components/common/pageWrapper';
 import { AsyncWrapper } from '$components/common/asyncWrapper';
 import { SearchBar } from '$components/specific/search/searchBar';
+import { Box } from '$components/common/box';
+import { ProductTable } from '$components/specific/product/productTable';
 import { useApiClient, useQuery } from '$components/hooks/api.tsx';
 
 const ProductSearchPage = () => {
   const client = useApiClient();
-  const { data, isLoading, error } = useQuery(client.products.searchProducts());
+  const { data: products, isLoading, error } = useQuery(client.products.searchProducts());
 
   return (
     <PageWrapper title="Product Search">
       <AsyncWrapper loading={isLoading} error={error}>
         <SearchBar />
 
-        {JSON.stringify(data, null, 2)}
+        <Box>
+          <ProductTable products={products ?? []} />
+        </Box>
       </AsyncWrapper>
     </PageWrapper>
   );
