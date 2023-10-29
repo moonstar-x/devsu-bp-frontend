038a81fd1f5f16526e8845d0ed68667f6e5fb7a8
moonstar-x <christian.lopez99@outlook.com>
2023-10-29 16:23:34 -0500

    Implemented local search for products table.

--- src/components/pages/ProductSearchPage.tsx
+++ src/components/pages/ProductSearchPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PageWrapper } from '$components/common/pageWrapper';
 import { AsyncWrapper } from '$components/common/asyncWrapper';
 import { SearchBar } from '$components/specific/search/searchBar';
@@ -7,16 +7,30 @@ import { ProductTable } from '$components/specific/product/productTable';
 import { useApiClient, useQuery } from '$components/hooks/api.tsx';
 
 const ProductSearchPage = () => {
+  const [searchTerm, setSearchTerm] = useState<string>('');
   const client = useApiClient();
   const { data: products, isLoading, error } = useQuery(client.products.searchProducts());
+
+  // Given that the API returns all objects, we're doing a local search.
+  const filteredProducts = (products ?? []).filter((product) => {
+    if (!searchTerm) {
+      return true;
+    }
+
+    return product.name.toLowerCase().includes(searchTerm.toLowerCase()) || product.description.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
+  const handleSearchChange = (value: string) => {
+    setSearchTerm(value);
+  };
 
   return (
     <PageWrapper title="Product Search">
       <AsyncWrapper loading={isLoading} error={error}>
-        <SearchBar />
+        <SearchBar onSearch={handleSearchChange} />
 
         <Box>
-          <ProductTable products={products ?? []} />
+          <ProductTable products={filteredProducts ?? []} />
         </Box>
       </AsyncWrapper>
     </PageWrapper>
