b2191f6b11662a3645230c77ac842edf9ea1afda
moonstar-x <christian.lopez99@outlook.com>
2023-10-29 22:50:26 -0500

    Implemented NotFoundPage.

--- src/components/pages/NotFoundPage.tsx
+++ src/components/pages/NotFoundPage.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { PageWrapper } from '$components/common/pageWrapper';
+import { Box } from '$components/common/box';
 
 const NotFoundPage = () => {
   return (
     <PageWrapper title="Page Not Found!">
-      NOT FOUND
+      <Box style={{ textAlign: 'center' }}>
+        <h1>
+          Page not found
+        </h1>
+
+        <p>
+          The page you were looking for does not exist.
+        </p>
+      </Box>
     </PageWrapper>
   );
 };
