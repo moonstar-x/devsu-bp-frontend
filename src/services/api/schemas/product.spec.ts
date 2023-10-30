2b45d996bf63f22b2c97bded31b4e26331367cf8
moonstar-x <christian.lopez99@outlook.com>
2023-10-29 21:24:57 -0500

    CreateSchema now validates for ID existence too.

--- src/services/api/schemas/product.spec.ts
+++ src/services/api/schemas/product.spec.ts
@@ -1,13 +1,17 @@
-import { ProductSchema } from './product.ts';
+import { createProductSchema } from './product.ts';
 import { ProductMutationFormBody } from '$services/api/models/product.ts';
+import { Client } from '$services/api/client/Client.ts';
+
+const client = new Client('', '');
+const checkProductSpy = jest.spyOn(client.products, 'checkProduct').mockResolvedValue(false);
 
 describe('Schemas: Product', () => {
-  describe('ProductSchema', () => {
+  describe('createProductSchema', () => {
     const RELEASE_DATE = new Date();
     const REVISION_DATE = new Date();
     REVISION_DATE.setFullYear(REVISION_DATE.getFullYear() + 1);
 
-    const Schema = ProductSchema;
+    const Schema = createProductSchema(client);
     const validProduct: ProductMutationFormBody = {
       id: 'tj-crd',
       name: 'Tarjeta de Credito',
@@ -17,7 +21,7 @@ describe('Schemas: Product', () => {
       date_revision: REVISION_DATE.toISOString()
     };
 
-    it('should validate false against incomplete objects.', () => {
+    it('should validate false against incomplete objects.', async () => {
       const { id, name, description, logo, date_revision, date_release } = validProduct;
 
       const values = [
@@ -30,11 +34,11 @@ describe('Schemas: Product', () => {
       ];
 
       for (const value of values) {
-        expect(Schema.validate(value).error).toBeTruthy();
+        await expect(Schema.validateAsync(value)).rejects.toBeTruthy();
       }
     });
 
-    it('should validate false against bad id.', () => {
+    it('should validate false against bad id.', async () => {
       const values = [
         { ...validProduct, id: '' },
         { ...validProduct, id: null },
@@ -44,11 +48,11 @@ describe('Schemas: Product', () => {
       ];
 
       for (const value of values) {
-        expect(Schema.validate(value).error).toBeTruthy();
+        await expect(Schema.validateAsync(value)).rejects.toBeTruthy();
       }
     });
 
-    it('should validate false against bad name.', () => {
+    it('should validate false against bad name.', async () => {
       const values = [
         { ...validProduct, name: '' },
         { ...validProduct, name: null },
@@ -58,11 +62,11 @@ describe('Schemas: Product', () => {
       ];
 
       for (const value of values) {
-        expect(Schema.validate(value).error).toBeTruthy();
+        await expect(Schema.validateAsync(value)).rejects.toBeTruthy();
       }
     });
 
-    it('should validate false against bad description.', () => {
+    it('should validate false against bad description.', async () => {
       const values = [
         { ...validProduct, description: '' },
         { ...validProduct, description: null },
@@ -72,11 +76,11 @@ describe('Schemas: Product', () => {
       ];
 
       for (const value of values) {
-        expect(Schema.validate(value).error).toBeTruthy();
+        await expect(Schema.validateAsync(value)).rejects.toBeTruthy();
       }
     });
 
-    it('should validate false against bad logo.', () => {
+    it('should validate false against bad logo.', async () => {
       const values = [
         { ...validProduct, logo: '' },
         { ...validProduct, logo: null },
@@ -85,11 +89,11 @@ describe('Schemas: Product', () => {
       ];
 
       for (const value of values) {
-        expect(Schema.validate(value).error).toBeTruthy();
+        await expect(Schema.validateAsync(value)).rejects.toBeTruthy();
       }
     });
 
-    it('should validate false against bad date_release.', () => {
+    it('should validate false against bad date_release.', async () => {
       const now = new Date();
       const date = new Date(now.getFullYear() - 1, now.getMonth(), now.getDate() + 1);
 
@@ -101,11 +105,11 @@ describe('Schemas: Product', () => {
       ];
 
       for (const value of values) {
-        expect(Schema.validate(value).error).toBeTruthy();
+        await expect(Schema.validateAsync(value)).rejects.toBeTruthy();
       }
     });
 
-    it('should validate false against bad date_revision.', () => {
+    it('should validate false against bad date_revision.', async () => {
       const now = new Date();
 
       const values = [
@@ -116,12 +120,17 @@ describe('Schemas: Product', () => {
       ];
 
       for (const value of values) {
-        expect(Schema.validate(value).error).toBeTruthy();
+        await expect(Schema.validateAsync(value)).rejects.toBeTruthy();
       }
     });
 
-    it('should validate true against valid objects.', () => {
-      expect(Schema.validate(validProduct).error).toBeFalsy();
+    it('should validate false against existing id.', async () => {
+      checkProductSpy.mockResolvedValueOnce(true);
+      await expect(Schema.validateAsync(validProduct)).rejects.toBeTruthy();
+    });
+
+    it('should validate true against valid objects.', async () => {
+      await expect(Schema.validateAsync(validProduct)).resolves.toBeTruthy();
     });
   });
 });
--- src/services/api/schemas/product.ts
+++ src/services/api/schemas/product.ts
@@ -1,6 +1,7 @@
 import BaseJoi from 'joi';
 import JoiDate from '@joi/date';
 import dayjs from 'dayjs';
+import { Client } from '$services/api/client/Client.ts';
 import { ProductMutationFormBody } from '$services/api/models/product.ts';
 
 const Joi: typeof BaseJoi & ReturnType<typeof JoiDate> = BaseJoi.extend(JoiDate);
@@ -11,46 +12,57 @@ const ONE_YEAR_FROM_NOW_AS_DATE = new Date();
 ONE_YEAR_FROM_NOW_AS_DATE.setFullYear(ONE_YEAR_FROM_NOW_AS_DATE.getFullYear() + 1);
 const ONE_YEAR_FROM_NOW = dayjs(ONE_YEAR_FROM_NOW_AS_DATE).format('YYYY-MM-DD');
 
-export const ProductSchema = Joi.object<ProductMutationFormBody>({
-  id: Joi.string().min(3).max(10).required()
-    .messages({
-      'string.empty': 'ID cannot be empty.',
-      'any.required': 'ID cannot be empty.',
-      'string.min': 'ID must be at least 3 characters long.',
-      'string.max': 'ID must be shorter than 10 characters.'
-    }),
-  name: Joi.string().min(5).max(100).required()
-    .messages({
-      'string.empty': 'Name cannot be empty.',
-      'any.required': 'Name cannot be empty.',
-      'string.min': 'Name must be at least 5 characters long.',
-      'string.max': 'Name must be shorter than 100 characters.'
-    }),
-  description: Joi.string().min(10).max(200).required()
-    .messages({
-      'string.empty': 'Description cannot be empty.',
-      'any.required': 'Description cannot be empty.',
-      'string.min': 'Description must be at least 10 characters long.',
-      'string.max': 'Description must be shorter than 200 characters.'
-    }),
-  logo: Joi.string().uri().required()
-    .messages({
-      'string.empty': 'Logo cannot be empty.',
-      'any.required': 'Logo cannot be empty.',
-      'string.uri': 'Logo must be a valid URL.'
-    }),
-  date_release: Joi.date().min(NOW).utc().required()
-    .messages({
-      'date.empty': 'Release date cannot be empty.',
-      'date.base': 'Release date must be a valid date.',
-      'any.required': 'Release date cannot be empty.',
-      'date.min': 'Release date cannot be before today.'
-    }),
-  date_revision: Joi.date().min(ONE_YEAR_FROM_NOW).utc().required()
-    .messages({
-      'date.empty': 'Revision date cannot be empty.',
-      'date.base': 'Revision date must be a valid date.',
-      'any.required': 'Revision date cannot be empty.',
-      'date.min': 'Revision date cannot be before one year from today.'
-    })
-});
+export const createProductSchema = (client: Client) => {
+  const validateIdExists: BaseJoi.ExternalValidationFunction<string, unknown> = async (value, helpers) => {
+    const exists = await client.products.checkProduct(value);
+    if (exists) {
+      return helpers.message({ external: 'This ID already exists.' });
+    }
+
+    return value;
+  };
+
+  return Joi.object<ProductMutationFormBody>({
+    id: Joi.string().min(3).max(10).required().external(validateIdExists)
+      .messages({
+        'string.empty': 'ID cannot be empty.',
+        'any.required': 'ID cannot be empty.',
+        'string.min': 'ID must be at least 3 characters long.',
+        'string.max': 'ID must be shorter than 10 characters.'
+      }),
+    name: Joi.string().min(5).max(100).required()
+      .messages({
+        'string.empty': 'Name cannot be empty.',
+        'any.required': 'Name cannot be empty.',
+        'string.min': 'Name must be at least 5 characters long.',
+        'string.max': 'Name must be shorter than 100 characters.'
+      }),
+    description: Joi.string().min(10).max(200).required()
+      .messages({
+        'string.empty': 'Description cannot be empty.',
+        'any.required': 'Description cannot be empty.',
+        'string.min': 'Description must be at least 10 characters long.',
+        'string.max': 'Description must be shorter than 200 characters.'
+      }),
+    logo: Joi.string().uri().required()
+      .messages({
+        'string.empty': 'Logo cannot be empty.',
+        'any.required': 'Logo cannot be empty.',
+        'string.uri': 'Logo must be a valid URL.'
+      }),
+    date_release: Joi.date().min(NOW).utc().required()
+      .messages({
+        'date.empty': 'Release date cannot be empty.',
+        'date.base': 'Release date must be a valid date.',
+        'any.required': 'Release date cannot be empty.',
+        'date.min': 'Release date cannot be before today.'
+      }),
+    date_revision: Joi.date().min(ONE_YEAR_FROM_NOW).utc().required()
+      .messages({
+        'date.empty': 'Revision date cannot be empty.',
+        'date.base': 'Revision date must be a valid date.',
+        'any.required': 'Revision date cannot be empty.',
+        'date.min': 'Revision date cannot be before one year from today.'
+      })
+  });
+};
